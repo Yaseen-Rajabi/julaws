83b170281e2f0c7921b92ffe6a81758fc22edefe
Yaseen Rajabi <52135608+AmbehTeam@users.noreply.github.com>
2023-01-11 00:09:16 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,20 +1,16 @@
 <?php
 
+	$dbHost = 'localhost';
+	$dbUser = 'root';
+	$dbPass = '';
+	$dbName = 'ju_laws_portal';
+
+$dbConn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName) or die ('MySQL connect failed. ' . mysqli_error());
+mysqli_select_db($dbConn,$dbName) or die('Cannot select database. ' . mysqli_error());
 
 mysqli_set_charset($dbConn,'utf8'); 
 
 date_default_timezone_set('Asia/Amman');
-
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
 
