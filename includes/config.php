96b7f05627b117c621dece32eb1145166be58a42
Yaseen Rajabi <52135608+AmbehTeam@users.noreply.github.com>
2023-01-11 01:04:49 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 	$dbHost = 'eu-cdbr-west-03.cleardb.net';
 	$dbUser = 'b27beb21fb1d11';
 	$dbPass = 'dcd38a91';
-	$dbName = 'ju_laws_portal';
+	$dbName = 'heroku_f16a9c545956ab9';
 
 $dbConn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName) or die ('MySQL connect failed. ' . mysqli_error());
 mysqli_select_db($dbConn,$dbName) or die('Cannot select database. ' . mysqli_error());
